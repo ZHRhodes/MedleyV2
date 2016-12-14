b8c32ad7ea11acab54115bcbea3e85991cd5a318
Zack Rhodes <zhrhodes@gmail.com>
2016-12-14 11:12:46 -0600

    Initial Commit

---
+++ Medley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Medley
+//
+//  Created by Zack Rhodes on 12/14/16.
+//  Copyright © 2016 zhrhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
