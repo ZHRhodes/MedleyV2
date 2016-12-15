88b0e79fb41055fb41289da62ea6a8c239a8b28b
Zack Rhodes <zhrhodes@gmail.com>
2016-12-15 08:47:57 -0600

    initial project setup, Facebook and Firebase login working

--- Medley/AppDelegate.swift
+++ Medley/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreData
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		FIRApp.configure()
+		FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 		return true
+	}
+	
+	func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+		print(url)
+		return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
 	}
 
 	func applicationWillResignActive(_ application: UIApplication) {
--- Medley/ViewController.swift
+++ Medley/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		if((FBSDKAccessToken.current()) != nil){
+			print("already logged on!")
+		}
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	@IBAction func facebookLoginClick(_ sender: Any) {
+		
+		let facebookLogin = FBSDKLoginManager()
+		print("logging in")
+		
+		facebookLogin.logIn(withReadPermissions: ["email", "public_profile"], from: self, handler: {
+			(facebookResult, facebookError) -> Void in
+			if facebookError != nil {
+				print("Facebook login failed. Error \(facebookError)")
+			}else if (facebookResult?.isCancelled)! {
+				print("Facebook login was cancelled")
+			}else {
+				print("Facebook login successful")
+				let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+				FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+					if let error = error {
+						print("Firebase Facebook login failed. Error \(error)")
+					}else {
+						print("Firebase Facebook login successful. User \(user)")
+					}
+				})
+			}
+		})
+		
+	}
+	
 
 
 }
