cb5e09333c35e6681c1e44ae343d4aa4bcb737ca
Zack Rhodes <zhrhodes@gmail.com>
2017-03-28 20:35:42 -0500

    base view controller

---
+++ Medley/MedleyBaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MedleyBaseViewController.swift
+//  Medley
+//
+//  Created by Zack Rhodes on 3/12/17.
+//  Copyright © 2017 zhrhodes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MedleyBaseViewController: UIViewController {
+	
+	func showPostExplorer(_ post: MusicPost){
+		print("showing post: \(post.song)")
+		if let vc = self.storyboard?.instantiateViewController(withIdentifier: Constants.StoryboardIDs.PostExplorer){
+			let vc = vc as! PostExplorerViewController
+			let vm = PostExplorerViewModel(post.viewModel)
+			vc.set(viewModel: vm)
+			self.present(vc, animated: true) {
+				//
+			}
+		}
+	}
+	
+	func showUserProfile(_ user: User){
+		print("showing user: \(user.username)")
+		if let vc = self.storyboard?.instantiateViewController(withIdentifier: Constants.StoryboardIDs.UserProfile){
+			let vc = vc as! UserProfileViewController
+			let vm = UserProfileViewModel(user)
+			vc.set(viewModel: vm)
+			self.present(vc, animated: true){
+				//
+			}
+		}
+	}
+	
+}
